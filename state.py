e1adb2ddb76fb415116f76bc335426d152bd66a4
JakimPL <jakimpl@gmail.com>
2021-06-21 14:43:50 +0200

    Added: board generator

--- state.py
+++ state.py
@@ -1,6 +1,10 @@
+import random
+
+
 class Board:
-    def __init__(self):
-        self._board = {}
+    def __init__(self, board=None, block_size=2):
+        self._board = board if board else {}
+        self._block_size = block_size
 
     def __call__(self, tile):
         return self._board[tile]
@@ -8,44 +12,79 @@ class Board:
     def __iter__(self):
         return self._board.__iter__()
 
+    def get_block_size(self):
+        return self._block_size
+
     def get_adjacent_tiles(self, tile, external=False):
         x, y = tile
         adjacent_tiles = []
-        for new_tile in [(x - 1, y - 1), (x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1), (x + 1, y + 1)]:
+        for new_tile in [(x - 1, y + 1), (x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1), (x + 1, y - 1)]:
             if (new_tile in self._board) ^ external:
                 adjacent_tiles.append(new_tile)
         return adjacent_tiles
 
     def is_tile_boundary(self, tile):
-        return len(self.get_adjacent_tiles(tile, False)) == 0
+        return len(self.get_adjacent_tiles(tile)) < 6
 
-    def generate_random(self, blocks, block_size=(2, 2)):
-        if blocks % (block_size[0] * block_size[1]) != 0:
-            raise ValueError('number of blocks is not divisible by the size of the block')
-
-        for i in range(block_size[0]):
-            for j in range(block_size[1]):
-                self._board[(i, j)] = 0
-        remaining_blocks = blocks - block_size[0] * block_size[1]
-
-        boundary_tiles = []
+    def get_random_block(self):
+        boundary_blocks = set()
         for tile in self._board:
             if self.is_tile_boundary(tile):
-                boundary_tiles.append(tile)
+                x, y = tile
+                block_lists = \
+                    [(x - self._block_size + i + 1, y - self._block_size) for i in range(self._block_size + 1)] + \
+                    [(x - self._block_size, y - self._block_size + i + 1) for i in range(self._block_size + 1)] + \
+                    [(x + 1, y - self._block_size + i) for i in range(1, self._block_size + 1)] + \
+                    [(x - self._block_size + i, y + 1) for i in range(1, self._block_size + 1)]
+
+                for block in block_lists:
+                    _x, _y = block
+                    success = True
+                    for i in range(self._block_size):
+                        for j in range(self._block_size):
+                            if (_x + i, _y + j) in self._board:
+                                success = False
+                                break
+
+                        if not success:
+                            break
+                    if success:
+                        boundary_blocks.add(block)
+
+        return random.choice(list(boundary_blocks))
+
+    def generate_board(self, blocks):
+        block_tiles = self._block_size * self._block_size
+        if blocks % block_tiles != 0:
+            raise ValueError('number of blocks is not divisible by the size of the block')
+
+        self.fill_block((0, 0))
+        remaining_blocks = blocks - block_tiles
+        while remaining_blocks > 0:
+            block = self.get_random_block()
+            self.fill_block(block)
+            remaining_blocks -= block_tiles
+
+    def fill_block(self, tile, value=0):
+        x, y = tile
+        for i in range(self._block_size):
+            for j in range(self._block_size):
+                self._board[(x + i, y + j)] = value
 
     def is_connected(self):
         pass
 
 
 class State:
-    def __init__(self, players: int, board=None):
+    def __init__(self, players: int, board=None, stack_size=16):
         if players < 1:
             raise ValueError('not enough players')
 
         self.players = players
         self.turn = 1
+        self.stack_size = stack_size
         if board is None:
             self.board = Board()
-            self.board.generate_random(self.players * 4)
+            self.board.generate_board(self.players * self.stack_size)
         else:
-            self.board = board
+            self.board = Board(board)
--- ui.py
+++ ui.py
@@ -7,7 +7,7 @@ class UI:
     def __init__(self):
         config = Config()
         self.line = 0
-        self.tile_size = 60
+        self.tile_size = 30
 
         self.game_width = 800
         self.game_height = 600
@@ -29,31 +29,25 @@ class UI:
         self.display = pygame.display.set_mode((self.game_width, self.game_height), 0, 32)
 
     def calculate_offsets(self, board):
-        min_x, min_y, max_x, max_y = None, None, None, None
+        xs = []
+        ys = []
         for tile in board:
             x, y = tile
-            _x, _y = x * 0.84 * self.tile_size, (y + 0.5 * x) * self.tile_size
-            if (min_x is None) or _x < min_x:
-                min_x = _x
-            if (max_x is None) or _x > max_x:
-                max_x = _x
-            if (min_y is None) or _y < min_y:
-                min_y = _y
-            if (max_y is None) or _y > max_y:
-                max_y = _y
+            xs.append(x * 0.84 * self.tile_size)
+            ys.append((y + 0.5 * x) * self.tile_size)
 
-        if not (min_x is None or max_x is None):
-            self.x_offset = (self.game_width - (max_x - min_x)) / 2
-
-        if not (min_y is None or max_y is None):
-            self.y_offset = (self.game_height - (max_y - min_y)) / 2
+        min_x, max_x = min(xs), max(xs)
+        min_y, max_y = min(ys), max(ys)
+        self.x_offset = (self.game_width - (max_x - min_x)) / 2 - min_x
+        self.y_offset = (self.game_height - (max_y - min_y)) / 2 - min_y
 
     def draw_board(self, state: State):
         pygame.draw.rect(self.display, self.colors[state.turn], (0, 0, self.game_width, self.game_height))
 
         for tile in state.board:
             x, y = tile
-            _x, _y = x * 0.84 * self.tile_size - self.new_rect[0] / 2, (y + 0.5 * x) * self.tile_size - self.new_rect[1] / 2
+            _x, _y = x * 0.84 * self.tile_size - self.new_rect[0] / 2, \
+                (y + 0.5 * x) * self.tile_size - self.new_rect[1] / 2
             self.display.blit(self.sprites[state.board(tile)], (_x + self.x_offset, _y + self.y_offset))
 
         pygame.display.update()
